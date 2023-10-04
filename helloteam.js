b1347c6f59d8945fb45c082e0074f47f8b7b486b
Enest Dembedza <enestdembedza@outlook.com>
2023-10-04 20:17:45 +0200

    updated helloteam.js

--- helloteam.js
+++ helloteam.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const teamNames = ["Glen","ThutaOkka"]; 
+const teamNames = ["Glen","ThutaOkka","Enest"]; 
 
 // Using a foreach() array method
 teamNames.forEach((name) => console.log(`Hello ${name}`));
